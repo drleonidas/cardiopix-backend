12a41039914a9237bba8601ee50c9eb01dfa5fe5
drleonidas <drleonidas5215@gmail.com>
2025-12-28 23:45:59 -0300

    Add physician responsibility mapping and validation

---
+++ cardiopix/__init__.py
@@ -0,0 +1,9 @@
+"""CardioPix backend core utilities."""
+
+from .report import Responsibility, LaudoPayloadBuilder, PhysicianIdentifiersMissing
+
+__all__ = [
+    "Responsibility",
+    "LaudoPayloadBuilder",
+    "PhysicianIdentifiersMissing",
+]
